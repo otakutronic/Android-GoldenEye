5a907ec0ee929270a4a2d522f4ef94097834302e
andrew.gardner <andrew.gardner@zozo.com>
2019-02-20 17:42:06 +0900

    Adding files

---
+++ goldeneye/src/main/java/co/infinum/goldeneye/models/CameraApi.kt
@@ -0,0 +1,10 @@
+package co.infinum.goldeneye.models
+
+/**
+ * Enum used to detect which camera API is used for
+ * implementation differences.
+ */
+enum class CameraApi {
+    CAMERA1,
+    CAMERA2
+}
